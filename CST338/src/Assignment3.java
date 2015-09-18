4b14bdf9d3018b7f1bc0bbb2b77f92222358546c
RyanND <Ryan@MappleBookPro.local>
2015-09-17 19:57:15 -0700

    Assignment 3 initial commit

---
+++ CST338/src/Assignment3.java
@@ -0,0 +1,24 @@
+/*
+   Team: Robert Contreras, Hyo Lee, Ryan Doherty
+   CST 338 - Fall 2015 Session A
+   Assignment 3, Card Game
+*/
+
+import java.util.*;
+import java.lang.Math;
+
+public class Assignment3
+{
+
+   public static void main(String[] args)
+   {
+      
+      Scanner keyboard = new Scanner(System.in);
+      keyboard.close();
+
+      System.out.println();
+   }
+   
+   
+}
+
